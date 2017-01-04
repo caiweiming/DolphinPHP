89d8ff8d6f649a02c5fa4bae9146d2f7cc9d4bb6
8966092 <8966092@163.com>
2017-01-04 20:51:06 +0800

    修复He'llWorld插件无法安装的问题

--- plugins/HelloWorld/config.php
+++ plugins/HelloWorld/config.php
@@ -13,100 +13,21 @@
  * 插件配置信息
  */
 return [
-    [
-        'name'    => 'status',
-        'title'   => '单选',
-        'type'    => 'radio',
-        'options' => [
-            '1' => '开启',
-            '0' => '关闭',
-        ],
-        'value' => '1',
-    ],
-    [
-        'name'  => 'text',
-        'title' => '单行文本',
-        'type'  => 'text',
-        'value' => 'x',
-        'tip'   => '提示',
-    ],
-    [
-        'name'  => 'textarea',
-        'title' => '多行文本',
-        'type'  => 'textarea',
-        'value' => '',
-        'tip'   => '提示',
-    ],
-    [
-        'name'    => 'checkbox',
-        'title'   => '多选',
-        'type'    => 'checkbox',
-        'options' => [
-            '1' => '是',
-            '0' => '否',
-        ],
-        'value' => '0',
-        'tip'   => '提示',
-    ],
-    [
-        'type'    => 'group',
-        'options' => [
+    ['radio', 'status', '单选', '', ['1' => '开启', '0' => '关闭'], 1],
+    ['text', 'text', '单行文本', '提示', 'x'],
+    ['textarea', 'textarea', '多行文本', '提示'],
+    ['checkbox', 'checkbox', '多选', '提示', ['1' => '是', '0' => '否'], 0],
+    ['group',
+        [
             '分组1' => [
-                [
-                    'name'    => 'status1',
-                    'title'   => '单选',
-                    'type'    => 'radio',
-                    'options' => [
-                        '1' => '开启',
-                        '0' => '关闭',
-                    ],
-                    'value' => '1',
-                ],
-                [
-                    'name'  => 'text1',
-                    'title' => '单行文本',
-                    'type'  => 'text',
-                    'value' => 'x',
-                    'tip'   => '提示',
-                ],
-                [
-                    'name'  => 'textarea1',
-                    'title' => '多行文本',
-                    'type'  => 'textarea',
-                    'value' => '',
-                    'tip'   => '提示',
-                ],
-                [
-                    'name'    => 'checkbox1',
-                    'title'   => '多选',
-                    'type'    => 'checkbox',
-                    'options' => [
-                        '1' => '是',
-                        '0' => '否',
-                    ],
-                    'value' => '0',
-                    'tip'   => '提示',
-                ],
+                ['radio', 'status1', '单选', '', ['1' => '开启', '0' => '关闭'], 1],
+                ['text', 'text1', '单行文本', '提示', 'x'],
+                ['array', 'textarea1', '多行文本2', '提示'],
+                ['checkbox', 'checkbox1', '多选', '提示', ['1' => '是', '0' => '否'], 0],
             ],
             '分组2' => [
-                [
-                    'name'  => 'textarea2',
-                    'title' => '多行文本',
-                    'type'  => 'textarea',
-                    'value' => '',
-                    'tip'   => '提示',
-                ],
-                [
-                    'name'    => 'checkbox2',
-                    'title'   => '多选',
-                    'type'    => 'checkbox',
-                    'options' => [
-                        '1' => '是',
-                        '0' => '否',
-                    ],
-                    'value' => '0',
-                    'tip'   => '提示',
-                ],
+                ['textarea', 'textarea2', '多行文本', '提示'],
+                ['checkbox', 'checkbox2', '多选', '提示', ['1' => '是', '0' => '否'], 0],
             ]
         ]
     ]
