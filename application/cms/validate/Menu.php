8fe966b20ac06d1c54d96afa391b2b69f43ea9f6
8966092 <8966092@163.com>
2017-04-17 01:23:40 +0800

    修正cms模块添加导航菜单时，数据验证规则不正确的问题

--- application/cms/validate/Menu.php
+++ application/cms/validate/Menu.php
@@ -22,18 +22,18 @@ class Menu extends Validate
 {
     // 定义验证规则
     protected $rule = [
-        'column|栏目'  => 'requireIf:type,0',
-        'page|单页'    => 'requireIf:type,1',
-        'title|菜单标题' => 'requireIf:type,2|length:1,30|unique:cms_nav',
-        'url|URL'     => 'requireIf:type,2',
+        'column|栏目' => 'requireIf:type,0',
+        'page|单页' => 'requireIf:type,1',
+        'title|菜单标题' => 'requireIf:type,2|length:1,30',
+        'url|URL' => 'requireIf:type,2',
     ];
 
     // 定义验证提示
     protected $message = [
-        'column'        => '请选择栏目',
-        'page'          => '请选择单页',
-        'title'         => '菜单标题不能为空',
-        'url.requireIf' => 'URL不能为空'
+        'column.requireIf' => '请选择栏目',
+        'page.requireIf'   => '请选择单页',
+        'title.requireIf'  => '菜单标题不能为空',
+        'url.requireIf'    => 'URL不能为空'
     ];
 
     // 定义验证场景
