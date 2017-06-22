672403baa540ced4af902063f134f88653149dfb
8966092 <8966092@163.com>
2017-06-22 09:14:38 +0800

    修正一处配置

--- application/extra/module.php
+++ application/extra/module.php
@@ -14,5 +14,5 @@
  */
 return [
     // 使用默认控制器层的模块
-    'default_controller_layer' => ['admin', 'index', 'common', 'extra', 'report']
+    'default_controller_layer' => ['admin', 'index', 'common', 'extra']
 ];
