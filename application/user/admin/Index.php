47de8f3b57541bbf3a07570daa0288c7448a0be3
ming <8966092@163.com>
2018-09-17 10:56:26 +0800

    修正非超级管理操作用户授权时报错的问题

--- application/user/admin/Index.php
+++ application/user/admin/Index.php
@@ -250,8 +250,7 @@ class Index extends Admin
         // 非超级管理员检查可编辑用户
         if (session('user_auth.role') != 1) {
             $role_list = RoleModel::getChildsId(session('user_auth.role'));
-            $map['role'] = ['in', $role_list];
-            $user_list = UserModel::where($map)->column('id');
+            $user_list = UserModel::where('role', 'in', $role_list)->column('id');
             if (!in_array($uid, $user_list)) {
                 $this->error('权限不足，没有可操作的用户');
             }
