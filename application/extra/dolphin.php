5494b178a3f077a148ebe6ea92755fa5c98d345c
ming <8966092@163.com>
2018-09-21 14:56:03 +0800

    更新版本号

--- application/extra/dolphin.php
+++ application/extra/dolphin.php
@@ -12,8 +12,8 @@
 return [
     // 产品信息
     'product_name'      => 'DolphinPHP',
-    'product_version'   => '1.3.0',
-    'build_version'     => '201805121805',
+    'product_version'   => '1.3.1',
+    'build_version'     => '201809181126',
     'product_website'   => 'http://www.dolphinphp.com',
     'product_update'    => 'http://www.dolphinphp.com/checkUpdate',
     'develop_team'      => 'DolphinPHP',
