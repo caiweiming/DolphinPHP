013314d0e0df5cd8b2c771b7e1e199919bb917b6
8966092 <8966092@163.com>
2020-02-21 15:37:21 +0800

    更新版本号

--- config/dolphin.php
+++ config/dolphin.php
@@ -8,15 +8,15 @@
 // +----------------------------------------------------------------------
 
 return [
-    // 产品信息
-    'product_name'      => 'DolphinPHP',
-    'product_version'   => '1.4.4',
-    'build_version'     => '202001191201',
-    'product_website'   => 'http://www.dolphinphp.com',
-    'product_update'    => 'http://www.dolphinphp.com/checkUpdate',
-    'develop_team'      => 'DolphinPHP',
+	// 产品信息
+	'product_name' => 'DolphinPHP',
+	'product_version' => '1.4.5',
+	'build_version' => '202002211536',
+	'product_website' => 'http://www.dolphinphp.com',
+	'product_update' => 'http://www.dolphinphp.com/checkUpdate',
+	'develop_team' => 'DolphinPHP',
 
-    // 公司信息
-    'company_name'    => '广东卓锐软件有限公司',
-    'company_website' => 'http://www.zrthink.com',
+	// 公司信息
+	'company_name' => '广东卓锐软件有限公司',
+	'company_website' => 'http://www.zrthink.com',
 ];
