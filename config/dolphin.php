a28442a88e0ae5ce0f5f44bec5905c9aa842f7f2
8966092 <8966092@163.com>
2024-07-11 11:20:57 +0800

    更新版本号1.6.0

--- config/dolphin.php
+++ config/dolphin.php
@@ -10,8 +10,8 @@
 return [
 	// 产品信息
 	'product_name' => 'DolphinPHP',
-	'product_version' => '1.5.1',
-	'build_version' => '202205300914',
+	'product_version' => '1.6.0',
+	'build_version' => '202407111120',
 	'product_website' => 'http://www.dolphinphp.com',
 	'product_update' => 'http://www.dolphinphp.com/checkUpdate',
 	'develop_team' => 'DolphinPHP',
